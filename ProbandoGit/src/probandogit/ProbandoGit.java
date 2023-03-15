79ccb8172bd6db4623fd729643b0bd8e92c6bda9
Diego Maximiliano Cuitino <diegomaxi3@outlook.com.ar>
2023-03-15 17:26:56 -0300

    Prueba branch

---
+++ ProbandoGit/src/probandogit/ProbandoGit.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+package probandogit;
+import java.util.*;
+
+/**
+ *
+ * @author Diego
+ */
+public class ProbandoGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese su nombre completo: "  );
+        String nombre = sc.nextLine();
+        
+        System.out.println("Nombre: " + nombre);
+    }
+    
+}
